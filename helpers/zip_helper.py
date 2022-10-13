62fdc0cc0a3c70dc1f8d5b46deec83455ebcae1c
Alexander Pfleging <alexander.pfleging@gmail.com>
2022-10-12 23:01:21 -0500

    added unzip_file util to zip_helper

--- helpers/zip_helper.py
+++ helpers/zip_helper.py
@@ -1,4 +1,5 @@
 import requests
+from zipfile import ZipFile as zippy
 
 
 def download_zip(url, path, filename):
@@ -7,11 +8,20 @@ def download_zip(url, path, filename):
         f.write(zip.content)
 
 
+def unzip_file(path, filename, extract_location):
+    with zippy(f'{path}{filename}') as zip_ref:
+        zip_ref.extractall(extract_location)
+
+
 if __name__ == '__main__':
     print('Executing as standalone script')
 
     url = 'https://www.retrosheet.org/gamelogs/gl1871.zip'
     path = 'test_output/'
     filename = url.split('/')[-1]
-
     download_zip(url, path, filename)
+
+    path = 'test_output/'
+    filename = 'gl1871.zip'
+    loc = 'test_output/test_unzip'
+    unzip_file(path, filename, loc)
