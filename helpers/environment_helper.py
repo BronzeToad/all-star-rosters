71a82943a73f2f720aff1ec40000b129d2b61c00
Alexander Pfleging <alexander.pfleging@gmail.com>
2022-10-12 22:10:31 -0500

    created environment helper

---
+++ helpers/environment_helper.py
@@ -0,0 +1,20 @@
+from dotenv import dotenv_values
+from typing import Union
+
+
+def get_api_key_sportradar() -> Union[str, None]:
+    key = 'API_KEY_SPORTRADAR'
+    config = dotenv_values('.env')
+    try:
+        val = config[key]
+    except KeyError:
+        print(f'Could not find environment variable {key} in .env file')
+        val = None
+    finally:
+        return val
+
+
+if __name__ == '__main__':
+    print('Executing as standalone script')
+
+    print(get_api_key_sportradar())
