055f7d38aa57a8c9fc938fd85435c829494a8937
Alexander Pfleging <github@ajpf.me>
2022-10-30 11:32:41 -0500

    1.0.0: added mlb data bank class

--- models/baseball_data_bank.py
+++ models/baseball_data_bank.py
@@ -1,6 +1,20 @@
-'''
+from models.data_extractor import DataExtractor
+
 class BaseballDataBank(DataExtractor):
+    BASE_URL = 'https://raw.githubusercontent.com/chadwickbureau/baseballdatabank/master/'
     
-    def __init__(self, download_url, location, filename):
-        super().__init__(download_url, location, filename)
-'''    
+    def __init__(self, download_url, location, filename, filetype, filepath,
+                 data_type: str = None):
+        
+        super().__init__(download_url, location, filename, filetype, filepath)
+        
+        self.data_type = data_type
+        
+    
+    @property
+    def data_type(self):
+        return self._data_type
+    
+    @data_type.setter
+    def data_type(self, val):
+        
