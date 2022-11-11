3f72a67d6b6ba85f64d9efcf2656c6249a0e2d45
BronzeToad <github@ajpf.me>
2022-11-11 09:00:03 -0600

    1.0.1: BaseballDataBank

--- helpers/env_utils.py
+++ helpers/env_utils.py
@@ -1,17 +1,34 @@
-from typing import Union
-from dotenv import dotenv_values
+import os.path as osPath
+from dotenv import dotenv_values as dotenvVals
+from pathlib import Path as libPath
+
+# ============================================================================ #
+
+def get_env_file() -> str:
+    return osPath.join(libPath(__file__).parent.parent, '.env')
 
 
-def get_env_variable(key: str) -> Union[str, None]:
-    config = dotenv_values('.env')
-    try:
-        val = config[key]
-    except KeyError:
-        print(f'Could not find environment variable {key} in .env file')
-        val = None
-    finally:
-        return val
+def get_env_vars(env_file: str = None) -> dict:
+    env_file = get_env_file() if env_file is None else env_file
+    if not libPath(env_file).is_file():
+        raise FileNotFoundError(f'Unable to locate .env file: {env_file}')
+    return dotenvVals(env_file)
 
+# ============================================================================ #
+
+def get_root_dir(env_file: str = None) -> str:
+    env_vars = get_env_vars(env_file)
+    return env_vars['WORKSPACE_DIR']
+
+
+def get_env_var(key: str, env_file: str = None) -> str:
+    env_vars = get_env_vars(env_file)
+    if key not in env_vars:
+        raise KeyError(f'Could not find environment variable {key} in .env file...')
+    return env_vars[key]
+
+# ============================================================================ #
 
 if __name__ == '__main__':
-    print('\n\n------------------------------------------------')
+    print('\n\n-------------------------- Executing as standalone script...')
+    
--- models/baseball_databank.py
+++ models/baseball_databank.py
@@ -1,56 +1,109 @@
-import os
-
+import os.path as osPath
+import helpers.env_utils as EnvHelper
 import helpers.data_utils as DataHelper
-from models.data_extracts import Downloader
 
-ROOT_DIR = os.getenv("ROOT_DIR")
+import icecream as ic
 
-class BaseballDataBank(Downloader):
+# ============================================================================ #
+
+class BaseballDataBank:
+
     BASE_URL = 'https://raw.githubusercontent.com/chadwickbureau/baseballdatabank/master'
+    ROOT_DIR = EnvHelper.get_root_dir()
+    CONFIG_DIR = osPath.join(ROOT_DIR, 'configs')
+    SAVE_DIR = osPath.join(ROOT_DIR, 'data/baseball_databank/downloads')
+    FILENAME_DICT = DataHelper.get_json(CONFIG_DIR, 'baseball_databank')
     
-    def __init__(self, url: str, location: str = None):
-        super().__init__(url, location)
+    def __init__(self, **kwargs):
+        
+        params = {
+            'contrib_files': None,
+            'core_files': None,
+            'upstream_files': None,
+        }
+        
+        params.update(kwargs)
+        
+        self.contrib_files = params['contrib_files']
+        self.core_files = params['core_files']
+        self.upstream_files = params['upstream_files']
+
+  # ============================================================================ #       
   
     @property
-    def config(self):
-        return DataHelper.get_json(f'{ROOT_DIR}/configs', 'mlb_data_bank')
-  
-    @property
-    def contrib_files(self):
-        return self.config['dataType']['contrib']
+    def contrib_files(self) -> list:
+        return self._contrib_files
     
-    @property
-    def core_files(self):
-        return self.config['dataType']['core']
-    
-    @property
-    def upstream_files(self):
-        return self.config['dataType']['upstream']
+    @contrib_files.setter
+    def contrib_files(self, val) -> None:
+        if val is None:
+            val = self.FILENAME_DICT['fileType']['contrib']      
+        self._contrib_files = val
+
 
     @property
-    def url_list(self):
-        _url_list = []
-        [_url_list.append(f'{self.BASE_URL}/contrib/{f}.csv') for f in self.contrib_files]
-        [_url_list.append(f'{self.BASE_URL}/core/{f}.csv') for f in self.core_files]
-        [_url_list.append(f'{self.BASE_URL}/upstream/{f}.csv') for f in self.upstream_files]
-        return _url_list
+    def core_files(self) -> list:
+        return self._core_files
     
-    '''     FIXME
-    def get_data(self):
-        if len(self.url_list) == 0:
-            self.get_urls()
-        for url in self.url_list:
-            DataHelper.download_from_url(url=url, folder='data/baseball_databank')
-    '''
+    @core_files.setter
+    def core_files(self, val) -> None:
+        if val is None:
+            val = self.FILENAME_DICT['fileType']['core']      
+        self._core_files = val
+        
+        
+    @property
+    def upstream_files(self) -> list:
+        return self._upstream_files
+    
+    @upstream_files.setter
+    def upstream_files(self, val) -> None:
+        if val is None:
+            val = self.FILENAME_DICT['fileType']['upstream']      
+        self._upstream_files = val
+
+  # ============================================================================ #
+  
+    @property
+    def contrib_urls(self) -> list:
+        return [f'{self.BASE_URL}/contrib/{f}.csv' for f in self.contrib_files]
+  
+    @property
+    def core_urls(self):
+        return [f'{self.BASE_URL}/core/{f}.csv' for f in self.core_files]
+    
+    @property
+    def upstream_urls(self):
+        return [f'{self.BASE_URL}/upstream/{f}.csv' for f in self.upstream_files]
+
+    @property
+    def download_urls(self):
+        _urls = []
+        for url in [self.contrib_urls, self.core_urls, self.upstream_urls]:
+            _urls.extend(url)
+        return _urls
+    
+# ============================================================================ #
+
+    @classmethod
+    def download(cls, **kwargs) -> None:
+        _cls = cls(**kwargs)
+        
+        _success = 0
+        for url in _cls.download_urls:
+            _get = DataHelper.download(url,
+                                       location=_cls.SAVE_DIR,
+                                       filename=DataHelper.get_filename_from_url(url))
+            
+            _success += 1 if _get else _success
+            
+        print(f'{_success} of {len(_cls.download_urls)} data files downloaded successfully to: {_cls.SAVE_DIR}')
+        
+        
+
+# ============================================================================ #
         
 if __name__ == '__main__':
     print('\n\n------------------------------------------------')
-    
 
-    tst = BaseballDataBank(url='https://rickastley.com/summerjamz.zip')
-
-
-    for url in tst.url_list:
-        print(url)
-    
-
+    BaseballDataBank.download()
